971bfe006b158eb2306f996f843c56e1dea3335d
YuriiStasiuk <58442181+YuriiStasiuk@users.noreply.github.com>
2021-08-20 00:32:51 +0300

    Create bot_templates.py
    New file for template functions

---
+++ bot_templates.py
@@ -0,0 +1,12 @@
+from aiogram import Bot, Dispatcher, executor, types
+
+def get_modules_list():
+    pass
+    #to do later
+
+def modules_keyboard():
+    modules = get_modules_list()
+    keyboard = types.InlineKeyboardMarkup()
+    for i in modules:
+        keyboard.add(types.InlineKeyboardButton(text="Модуль " + i[0], callback_data="select_module_" + i[1]))
+    return keyboard
