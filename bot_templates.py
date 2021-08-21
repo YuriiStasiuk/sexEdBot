b9ee0c3deaaf4a72c234520a0c1842251959826c
YuriiStasiuk <58442181+YuriiStasiuk@users.noreply.github.com>
2021-08-22 01:17:17 +0300

    Update bot_templates.py
    fixed mistakes

--- bot_templates.py
+++ bot_templates.py
@@ -1,23 +1,15 @@
 import asyncio
 from aiogram import Bot, Dispatcher, executor, types
 
-async def get_modules_list():
-    module = {'1': 1,'2': 2}
-    return module
+def get_modules_list():
+    modules = [['1', '1'], ['2', '2']]
+    return modules
 
 
-async def modules_template():
+def modules_template():
     message_text = "Обери модуль"
-    modules = asyncio.run(get_modules_list())
+    modules = get_modules_list()
     keyboard = types.InlineKeyboardMarkup()
     for i in modules:
         keyboard.add(types.InlineKeyboardButton(text="Модуль " + i[0], callback_data="select_module_" + i[1]))
     return message_text, keyboard
-
-
-# modules = [["Назва модулю", "Ідентифікатор модулю"], ['назва іншого модулю', "ідентифікатор іншого модулю"]]
- #   keyboard = types.InlineKeyboardMarkup()
-  #  for i in modules:
-   #     keyboard.add(types.InlineKeyboardButton(text="Модуль " + i[0], callback_data="select_module_"+i[1]))
-    #await message.reply(text = "Ось це треба буде пофіксити", reply_markup=types.ReplyKeyboardRemove())   #to fix later
-    #await message.answer("Обери модуль", reply_markup=keyboard)
